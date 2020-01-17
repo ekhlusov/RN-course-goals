f803610383b1980b4ca652f2f6373684c1b003a7
Хлусов Евгений Сергеевич <Khlusov.E.S@sberbank.ru>
2020-01-17 17:03:53 +0300

    initial

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [text, changeText] = useState("Мир");
+
+  const handleChange = () => changeText(text === 'Мир' ? "Витя" : 'Мир');
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>Привет {text}!!!!</Text>
+      <Text>Как дела?</Text>
+
+      <Button title="Поменять на Витя" onPress={handleChange} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
